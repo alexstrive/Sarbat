a656969febca9e13bb66c9c9ccb54a3909644ab0
allordiron <allordiron@gmail.com>
2016-08-10 18:14:36 +0300

    Init commit

---
+++ js/circles.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('#circle-photoshop').circleProgress({
+        value: 0.70,
+        size: 125,
+        fill: {
+            gradient: ["#802775", "#db071e"]
+        },
+        startAngle: Math.PI / 2
+    });
+    $('#circle-fireworks').circleProgress({
+        value: 0.90,
+        size: 125,
+        fill: {
+            gradient: ["#802775", "#db071e"]
+        },
+        startAngle: Math.PI / 2
+    });
+    $('#circle-html').circleProgress({
+        value: 0.60,
+        size: 125,
+        fill: {
+            gradient: ["#802775", "#db071e"]
+        },
+        startAngle: Math.PI / 2
+    });
+    $('#circle-wordpress').circleProgress({
+        value: 0.40,
+        size: 125,
+        fill: {
+            gradient: ["#802775", "#db071e"]
+        },
+        startAngle: Math.PI / 2
+    });
+});
